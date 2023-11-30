6d7b433fe038ef78686c4d87f6524eb02aad77b6
Angga Aditya <agungangga2001@gmail.com>
2023-11-30 17:17:24 +0800

    routing sidebar

--- src/component/sideBar/SideBarMenu.js
+++ src/component/sideBar/SideBarMenu.js
@@ -5,28 +5,40 @@ export default function SideBarMenu() {
     <div className='flex text-white font-bold text-3xl'>
         <ul className='space-y-7'>
             <li className='flex'>
-                <img src='./assets/dashboard.svg' alt='dahsboard' className='pr-3 '/>
-                dashboard
+                <a href='/' className='flex'>
+                    <img src='./assets/dashboard.svg' alt='dahsboard' className='pr-3 '/>
+                    dashboard
+                </a>
             </li>
             <li className='flex'>
-                <img src='./assets/offer.svg' alt='offer' className='pr-3'/>
-                offer
+                <a href='/' className='flex'>
+                    <img src='./assets/offer.svg' alt='offer' className='pr-3'/>
+                    offer
+                </a>
             </li>
             <li className='flex'>
-                <img src='./assets/news.svg' alt='news' className='pr-3'/>
-                News
+                <a href='/' className='flex'>
+                    <img src='./assets/news.svg' alt='news' className='pr-3'/>
+                    News
+                </a>
             </li>
             <li className='flex'>
-                <img src='./assets/gallery.svg' alt='gallery' className='pr-3'/>
-                Gallery
+                <a href='/' className='flex'>
+                    <img src='./assets/gallery.svg' alt='gallery' className='pr-3'/>
+                    Gallery
+                </a>
             </li>
             <li className='flex'>
-                <img src='./assets/order.svg' alt='order' className='pr-3'/>
-                Order
+                <a href='/' className='flex'>
+                    <img src='./assets/order.svg' alt='order' className='pr-3'/>
+                    Order
+                </a>
             </li>
             <li className='flex'>
-                <img src='./assets/tiket.svg' alt='tiket' className='pr-3'/>
-                Tiket
+                <a href='/' className='flex'>
+                    <img src='./assets/tiket.svg' alt='tiket' className='pr-3'/>
+                    Tiket
+                </a>
             </li>
         </ul>
     </div>
